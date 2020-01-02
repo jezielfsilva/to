f5d06b520be30b46c49739f90940b7bbe6ae4f5a
Jeziel Silva <jezielfsilva@outlook.com>
2020-01-02 18:20:13 -0300

    novas funções adicionadas aos inputs e novo component criado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PageSignup from './components/createAccount';
+import PageLoggin from './components/loggin';
 
 function App() {
   return (
     <div className="App">
       <PageSignup/>
+      <PageLoggin/>
     </div>
   );
 }
--- src/components/loggin.js
+++ src/components/loggin.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
-class PageSignup extends React.Component {
+class PageLoggin extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            nome: '',
             email: '',
             senha: null,
         }
-    }
-
-    getname = (event) => {
-        this.setState({
-            nome: event.target.value,
-        })
     }
 
     getemail = (event) => {
@@ -32,25 +25,21 @@ class PageSignup extends React.Component {
 
     render () {
         return (
-            <div className='box-login'>
+            <div>
                 <form>
-                    <label>nome:
-                        <input onChange={this.getname} type="text" placeholder="seu nome aqui" />
-                    </label>
-                    <p>{this.state.nome}</p>
                     <label>email:
-                        <input onChange={this.getemail} type="email" placeholder="seu email aqui" />
+                        <input onChange={this.getemail} type="email" placeholder="seu email" />
                     </label>
                     <p>{this.state.email}</p>
                     <label>senha:
-                        <input onChange={this.getpassword} type="password" placeholder="sua senha aqui" />
+                        <input onChange={this.getpassword} type="password" placeholder="sua senha" />
                     </label>
                     <p>{this.state.senha}</p>
-                    <button>Signup</button>
+                    <button>login</button>
                 </form>
             </div>
         )
     }
 }
 
-export default PageSignup;
+export default PageLoggin;
